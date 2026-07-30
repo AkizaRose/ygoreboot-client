20eb1d9e4878e8555282eb8969083bd73650f9fd
Zauls <wsbryant96@gmail.com>
2026-07-30 15:29:36 +0100

    Improved layout and visuals

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import CardBrowser from './components/CardBrowser/CardBrowser';
 import cardData from './data/carddata.json';
 import type { CardData } from './types/Card';
+import './App.css';
 
 function App() {
   const cards = cardData as CardData[];
 
-  return <CardBrowser cards={cards} />;
+  return (
+    <div className="PageContent">
+      <CardBrowser cards={cards} />
+    </div>
+  );
 }
 
 export default App;
